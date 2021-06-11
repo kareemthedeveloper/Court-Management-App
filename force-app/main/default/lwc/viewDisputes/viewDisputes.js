df03d83798ab8e545d30df129a79046bbb3fd675
kareemthedeveloper <kareem.elsayed@smoothstack.com>
2021-06-11 02:32:12 -0400

    Using a testdatafactory and lwc tweaks

--- force-app/main/default/lwc/viewDisputes/viewDisputes.js
+++ force-app/main/default/lwc/viewDisputes/viewDisputes.js
@@ -5,19 +5,19 @@ const DELAY = 300;
 
 
 export default class ViewDisputes extends LightningElement {
-
+  
     searchKey = "";
-
+    
     @wire(findDisputes,{searchKey:'$searchKey'})disputes;
     handleKeyChange(event) {
-        // Debouncing this method: Do not update the reactive property as long as this function is
-        // being called within a delay of DELAY. This is to avoid a very large number of Apex method calls.
+        
         window.clearTimeout(this.delayTimeout);
         const searchKey = event.target.value;
         console.log(this.disputes);
-        // eslint-disable-next-line @lwc/lwc/no-async-operation
+
         this.delayTimeout = setTimeout(() => {
             this.searchKey = searchKey;
         }, DELAY);
     }
+    
 }
